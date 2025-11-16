bbedda5b184d19b7ff05116d8674425701114494
Anawrulkabir <mdanawrulkabirfahad123@gmail.com>
2025-11-16 16:07:34 +0600

    fix

--- update-lockfile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Script to update package-lock.json with missing dependencies
-
-echo "Updating package-lock.json..."
-npm install
-
-echo "Done! The package-lock.json has been updated."
-echo "Please commit and push the updated package-lock.json file."
-
